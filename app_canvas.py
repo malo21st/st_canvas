e7b2e69657c0239d06ec63d5fc2821a66a38996b
malo21st <44739330+malo21st@users.noreply.github.com>
2021-04-01 22:17:53 +0900

    Update app_canvas.py

--- app_canvas.py
+++ app_canvas.py
@@ -2,6 +2,9 @@ import pandas as pd
 from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+import qrcode
+
+URL = 'https://share.streamlit.io/malo21st/st_canvas/main/app_canvas.py'
 
 # Create a canvas component
 canvas_result = st_canvas(
@@ -14,3 +17,5 @@ canvas_result = st_canvas(
     drawing_mode="freedraw",
     key="canvas",
 )
+
+qrcode.make(URL)
