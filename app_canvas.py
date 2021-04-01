dad2ea477814ab1dc5bcd7a0578aad92158a427f
malo21st <44739330+malo21st@users.noreply.github.com>
2021-04-01 21:53:12 +0900

    Update app_canvas.py

--- app_canvas.py
+++ app_canvas.py
@@ -3,31 +3,19 @@ from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-# Specify canvas parameters in application
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
-)
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
-
 # Create a canvas component
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color="" if bg_image else bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
-    update_streamlit=realtime_update,
-    height=150,
-    drawing_mode=drawing_mode,
+    stroke_width=10,
+    stroke_color="black",
+    background_color="lightblue",
+    update_streamlit=True,
+    height=200,
+    width =200,
+    drawing_mode="freedraw",
     key="canvas",
 )
 
 # Do something interesting with the image data and paths
 if canvas_result.image_data is not None:
     st.image(canvas_result.image_data)
-if canvas_result.json_data is not None:
-    st.dataframe(pd.json_normalize(canvas_result.json_data["objects"]))
